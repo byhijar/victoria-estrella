4caf48d9f21ec8382bf54b2b3d9d29b17be4e762
Jose Hijar <byhijar@gmail.com>
2026-03-15 21:40:43 -0300

    🐛 Hotfix: Importación de useEffect faltante en Materials y estabilidad

--- src/pages/Materials.jsx
+++ src/pages/Materials.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useMaterials } from '../hooks/useMaterials';
 import { addMaterial, deleteMaterial, updateMaterial, wipeAllData } from '../services/materialService';
 import { restockMaterial } from '../services/saleService';
